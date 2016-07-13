0f1985b13766f146036a242c1f6d93e6e19e77b1
徐玄清 <524058708@qq.com>
2016-07-13 17:35:27 +0800

    测试提交功能

--- src/Test2.java
+++ src/Test2.java
@@ -1,8 +1,8 @@
 /**
  * Created by thinkpad on 2016/7/13.
  */
-public class Test {
+public class Test2 {
     public static void main(String[] args) {
-        System.out.println("中文");
+        System.out.println("我就是测试");
     }
 }
